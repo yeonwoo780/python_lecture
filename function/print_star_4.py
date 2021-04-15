50edcc863aefa82d54990467b4496076014e042d
Jung <rickrl0315db@gmail.com>
2021-04-15 17:29:31 +0900

    func_1

---
+++ function/print_star_4.py
@@ -0,0 +1,14 @@
+def print_star():
+    print('************************')
+print_star() # 별표 출력함수 호출 1
+print_star() # 별표 출력함수 호출 2
+print_star() # 별표 출력함수 호출 3
+print_star() # 별표 출력함수 호출 4
+
+print("\n")
+
+def print_star6():
+    for i in range(0,6):
+        print('************************')
+
+print_star6()
---
+++ function/print_star_func.py
@@ -0,0 +1,5 @@
+def print_star(a, c, b = 5): #a, b 파라미터
+    print('************************', a, b, c)
+
+print_star(5, 6)
+print_star(c = 7, a = 10)
