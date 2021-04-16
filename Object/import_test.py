88111645da1cab86a184b2502f1788bd36f40b1a
Jung <rickrl0315db@gmail.com>
2021-04-16 17:44:36 +0900

    Object_1

---
+++ Object/import_test.py
@@ -0,0 +1,7 @@
+import fourcal_class_1 as fourcal
+from fourcal_class_1 import FourCal as FC
+
+b = FC(4,2)
+c = b.add()
+print(c)
+
---
+++ moduleimport/numpytest.py
@@ -0,0 +1,4 @@
+import numpy as np
+b = np.array([1,2,3])
+print(type(b))
+print(b)
